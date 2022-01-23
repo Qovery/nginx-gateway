fa2d91ff409afc1320bd52e03b51d3dfd47f03c6
Romaric Philogene <evoxmusic@gmail.com>
2022-01-23 13:25:40 +0100

    feat: improve run.sh

--- run.sh
+++ run.sh
@@ -11,16 +11,30 @@ envsubst '$KUBERNETES_DNS_SERVICE_IP' < /etc/nginx/nginx.conf.template > /etc/ng
 
 cp /etc/nginx/routes.conf.template /etc/nginx/routes.conf
 
+fqdn=$(grep search /etc/resolv.conf | awk '{print $2}' | head -1)
+
+if [ -z "$fqdn" ]; then
+  fqdn='local'
+fi
+
 for var in $(env)
 do
   ENV_VAR_KEY="\$$(echo "$var" | tr "=" " " | awk '{print $1}')"
-  CMD="envsubst '$ENV_VAR_KEY' < /etc/nginx/routes.conf"
-  # do not remove - need to buffer output to rewrite in the file
-  OUTPUT=$(eval "$CMD")
-  echo "$OUTPUT" > /etc/nginx/routes.conf
+  ENV_VAR_VALUE=$(echo "$var" | tr "$ENV_VAR_KEY=" " " | awk '{print $1}')
+  CMD="sed -i 's/$ENV_VAR_KEY/$(echo "$ENV_VAR_VALUE" | tr "/" "\/").$fqdn/g' /etc/nginx/routes.conf"
+
+  case "$ENV_VAR_VALUE" in
+  *.*)
+    # there is a domain - so do not concatenate with fqdn
+    CMD="sed -i 's/$ENV_VAR_KEY/$(echo "$ENV_VAR_VALUE" | tr "/" "\/")/g' /etc/nginx/routes.conf"
+    ;;
+  esac
+
+  eval "$CMD"
 done
 
-printf "nameserver: %s\n\n" $KUBERNETES_DNS_SERVICE_IP
+printf "nameserver: %s\n" "$KUBERNETES_DNS_SERVICE_IP"
+printf "search FQDN: %s\n\n" "$fqdn"
 
 printf "--------- GENERATED ROUTES.CONF ------------\n"
 cat /etc/nginx/routes.conf
