1f70aede1f5f9936c72327992ce2b4167c6fe7f7
Nicolas Albani <nicolasalbani@gmail.com>
2022-09-14 20:23:08 -0300

    separating repo into prod & stage

---
+++ staging/README.md
@@ -0,0 +1,21 @@
+# NGINX API Gateway
+
+This repository contains everything you need to deploy an API gateway for your apps on Qovery.
+
+[Read this tutorial](https://hub.qovery.com/guides/tutorial/use-an-api-gateway-in-front-of-multiple-services/) to learn how to use this API Gateway.
+
+## Usage
+
+[NGINX](https://www.nginx.com/) is an open-source web server that can also be used as a reverse proxy, load balancer, mail proxy and HTTP cache.
+
+1. Clone this repository
+2. Modify `routes.conf.template`
+
+Check out the [NGINX documentation](https://nginx.org/en/docs/beginners_guide.html) for more advanced configuration.
+
+### Environment variables
+
+| Key | Default Value | Mandatory |
+|-----|---------------|-----------|
+|KUBERNETES_DNS_SERVICE_IP | 127.0.0.1   | No
+
---
+++ staging/run.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$KUBERNETES_DNS_SERVICE_IP" ]; then
+  nameserver=$(grep nameserver /etc/resolv.conf | awk '{print $2}' | head -1)
+  export KUBERNETES_DNS_SERVICE_IP=$nameserver
+fi
+
+envsubst '$KUBERNETES_DNS_SERVICE_IP' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+cp /etc/nginx/routes.conf.template /etc/nginx/routes.conf
+
+envsubst "$(printf '${%s} ' $(env | cut -d'=' -f1))" < /etc/nginx/conf.d/http-server.conf.template > /etc/nginx/conf.d/http-server.conf
+
+fqdn=$(grep search /etc/resolv.conf | awk '{print $2}' | head -1)
+
+if [ -z "$fqdn" ]; then
+  fqdn='local'
+fi
+
+for var in $(env)
+do
+  ENV_VAR_KEY="\$$(echo "$var" | tr "=" " " | awk '{print $1}')"
+  ENV_VAR_VALUE=$(echo "$var" | tr "$ENV_VAR_KEY=" " " | awk '{print $1}')
+
+  case "$ENV_VAR_VALUE" in
+  *.*)
+    # there is a domain - so do not concatenate with fqdn
+    CMD="sed -i 's/$ENV_VAR_KEY/$(echo "$ENV_VAR_VALUE" | tr "/" "\/")/g' /etc/nginx/routes.conf"
+    ;;
+  *)
+    local_port="$(dig +noall +answer +time=3 +tries=1 srv \*._tcp."$ENV_VAR_VALUE"."$fqdn" | awk '{print $7}')"
+    ## TODO check local_port is a valid port number
+    CMD="sed -i 's/$ENV_VAR_KEY/$(echo "$ENV_VAR_VALUE" | tr "/" "\/").$fqdn:$local_port/g' /etc/nginx/routes.conf"
+    ;;
+  esac
+
+  eval "$CMD"
+done
+
+printf "nameserver: %s\n" "$KUBERNETES_DNS_SERVICE_IP"
+printf "search FQDN: %s\n\n" "$fqdn"
+
+printf "--------- GENERATED ROUTES.CONF ------------\n"
+cat /etc/nginx/routes.conf
+printf "--------- EOF GENERTED ROUTES.CONF ---------\n"
+
+printf "> nginx.conf and routes.conf configuration files loaded!\n\n"
+
+# Start NGINX
+nginx -g 'daemon off;'
